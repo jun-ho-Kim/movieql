bc806a5720339e856fccabd7e4daff56354e77dc
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-06-20 16:41:22 +0900

    Mutaion: Create+Delete

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,44 @@
-export const HorangFriend = [
+let movies = [
     {
-        id: "0",
-        name : "Horangee",
-        age: 22,
-        gender : "male"
+        id: 0,
+        name: "Star Wars - The new one",
+        score: 1
     },
     {
-        id: "1",
-        name : "Tokki",
-        age : 24,
-        gender : "femail",
+        id: 1,
+        name: "Avengers - The new one",
+        score: 8
     },
     {
-        id: "2",
-        name : "strawberry",
-        age: 24,
-        gender: "femail"
+        id: 3,
+        name: "Logan",
+        score: 2
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = HorangFriend.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filteredMovie = movies.filter(movie => movie.id = id);
+    return filteredMovie[0];
 }
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: parseInt(`${movies.length +1}`),
+        name,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
+}
+
+export const deleteMovie = id => {
+    const cleanMovie = movies.filter(movie => movie.id !== id)
+    if(movies.length > cleanMovie.length) {
+        movies = cleanMovie;
+        return true;
+    } else {
+        return false;
+    }
+};
